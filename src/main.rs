6f178ae8a08417974f64d36a34a02faee644ae39
Brendan Dagys <brendandagys@gmail.com>
2022-08-07 13:06:45 -0400

    More refactoring
    Use stack-allocated arrays where possible.
    
    Add iteration for `merged.append()` with `if-else` and then `match` approach.

--- src/main.rs
+++ src/main.rs
@@ -22,26 +22,34 @@ fn merge(left: &[i32], right: &[i32]) -> Vec<i32> {
         }
     }
 
-    let mut leftover = if i < left.len() {
-        left[i..left.len()].to_vec()
-    } else {
-        right[j..right.len()].to_vec()
-    };
+    // 1 - two statements
+    // 2 - if-else inside `append()`
+    // 3 - `match` inside `append()`
+    // let mut leftover = if i < left.len() {
+    //     left[i..left.len()].to_vec()
+    // } else {
+    //     right[j..right.len()].to_vec()
+    // };
 
-    merged.append(&mut leftover);
+    // merged.append(&mut leftover);
+
+    merged.append(&mut match i < left.len() {
+        true => left[i..left.len()].to_vec(),
+        false => right[j..right.len()].to_vec(),
+    });
 
     merged
 }
 
-fn merge_sort(vec: Vec<i32>) -> Vec<i32> {
-    if vec.len() == 1 {
-        vec
+fn merge_sort(arr: &[i32]) -> Vec<i32> {
+    if arr.len() == 1 {
+        arr.to_vec()
     } else {
-        let middle = vec.len() / 2;
+        let middle = arr.len() / 2;
         println!("Middle: {middle}");
 
-        let left = merge_sort(vec[0..middle].to_vec());
-        let right = merge_sort(vec[middle..].to_vec());
+        let left = merge_sort(&arr[0..middle]);
+        let right = merge_sort(&arr[middle..]);
 
         return merge(&left, &right);
     }
@@ -51,15 +59,15 @@ fn main() {
     print_time("STARTING");
 
     let mut rng = thread_rng();
-    let mut numbers: Vec<i32> = Vec::with_capacity(NUMBER_COUNT);
 
-    for _x in 0..numbers.capacity() {
-        numbers.push(rng.gen_range(1..MAX_RANDOM_NUMBER))
+    let mut arr: [i32; NUMBER_COUNT] = [-1; NUMBER_COUNT];
+    for i in 0..NUMBER_COUNT {
+        arr[i] = rng.gen_range(1..MAX_RANDOM_NUMBER);
     }
 
-    println!("NUMBERS: {:?}", numbers);
+    println!("NUMBERS: {:?}", arr);
 
-    let sorted_numbers = merge_sort(numbers);
+    let sorted_numbers = merge_sort(&arr);
     println!("{:?}", sorted_numbers);
 
     print_time("SORTED!");
